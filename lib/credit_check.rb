bed7f1c8b5beea9f74f7fcd51127673982f7a547
Tara Craig <tara.craig7@gmail.com>
2018-06-25 22:12:24 -0600

    Add tests, double every other number method.

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -6,12 +6,27 @@ class CreditCheck
     @cc_number = cc_number
   end
 
-  def validate
-
-
+  def validate(cc_number)
+    converted_number = separate_characters(cc_number)
+    doubled = double_every_other_number(converted_number)
+    nines = sum_greater_than_nine(doubled)
+    sum = nines.sum
+    validity = valid?(sum)
+    generate_message(validity)
   end
 
-  def separate_characters
-    cc_number.to_i.digits
+  def separate_and_reverse_characters
+    @cc_number.to_i.digits
   end
+
+  def double_every_other_number
+    @cc_number.map.with_index do |num, i|
+      if i.odd?
+        num * 2
+      else
+        num
+      end
+    end
+  end
+
 end
