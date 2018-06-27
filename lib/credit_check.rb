d160aefeaf773a86efc00dd571f79973e78f9e86
Tara Craig <tara.craig7@gmail.com>
2018-06-27 17:23:37 -0600

    Change methods to reflect spec sheet requirements

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,13 +1,7 @@
 class CreditCheck
-  #
-  attr_reader   :cc_number
-
-  def initialize(cc_number)
-    @cc_number = cc_number
-  end
 
   def valid_number?(card_number)
-    converted_number = separate_and_reverse_characters(cc_number)
+    converted_number = separate_and_reverse_characters(card_number)
     doubled = double_every_other_number(converted_number)
     nines = sum_greater_than_nine(doubled)
     sum = sum_numbers(nines)
@@ -15,20 +9,16 @@ class CreditCheck
   end
 
   def validation_output(card_number)
-    converted_number = separate_and_reverse_characters(cc_number)
-    doubled = double_every_other_number(converted_number)
-    nines = sum_greater_than_nine(doubled)
-    sum = sum_numbers(nines)
-    output = validity(sum)
-    validation(output)
+    valid = valid_number?(card_number)
+    validation(valid, card_number)
   end
 
   def separate_and_reverse_characters(card_number)
     card_number.to_i.digits
   end
 
-  def double_every_other_number(card_number)
-    card_number.map.with_index do |num, i|
+  def double_every_other_number(converted_number)
+    converted_number.map.with_index do |num, i|
       if i.odd?
         num * 2
       else
@@ -37,8 +27,8 @@ class CreditCheck
     end
   end
 
-  def sum_greater_than_nine(card_number)
-    card_number.map do |num|
+  def sum_greater_than_nine(doubled)
+    doubled.map do |num|
       if num > 9
         num - 9
       else
@@ -47,23 +37,23 @@ class CreditCheck
     end
   end
 
-  def sum_numbers(card_number)
-    card_number.sum
+  def sum_numbers(nines)
+    nines.sum
   end
 
-  def validity(card_number)
-    if card_number % 10 == 0
+  def validity(sum)
+    if sum % 10 == 0
       true
     else
       false
     end
   end
 
-  def validation(card_number)
-    if card_number == true
-      "The number #{@cc_number} is valid."
+  def validation(validity, credit_card)
+    if validity == true
+      "The number #{credit_card} is valid."
     else
-      "The number #{@cc_number} is invalid."
+      "The number #{credit_card} is invalid."
     end
   end
 end
--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -4,14 +4,14 @@ require './lib/credit_check'
 
 class CreditCheckTest < Minitest::Test
   def test_it_exists
-    credit_check = CreditCheck.new("4929735477250543")
+    credit_check = CreditCheck.new
 
     assert_instance_of CreditCheck, credit_check
   end
 
   def test_it_can_separate_and_reverse_characters
-    credit_check = CreditCheck.new("4929735477250543")
-    credit_card = "4929735477250543"
+    credit_check = CreditCheck.new
+    credit_card = 4929735477250543
 
     expected = [3, 4, 5, 0, 5, 2, 7, 7, 4, 5, 3, 7, 9, 2, 9, 4]
 
@@ -19,7 +19,7 @@ class CreditCheckTest < Minitest::Test
   end
 
   def test_it_can_double_every_other_number
-    credit_check = CreditCheck.new("4929735477250543")
+    credit_check = CreditCheck.new
     credit_card = [3, 4, 5, 0, 5, 2, 7, 7, 4, 5, 3, 7, 9, 2, 9, 4]
 
     expected = [3, 8, 5, 0, 5, 4, 7, 14, 4, 10, 3, 14, 9, 4, 9, 8]
@@ -27,7 +27,7 @@ class CreditCheckTest < Minitest::Test
   end
 
   def test_it_can_sum_digits_over_nine
-    credit_check = CreditCheck.new("4929735477250543")
+    credit_check = CreditCheck.new
     credit_card = [3, 8, 5, 0, 5, 4, 7, 14, 4, 10, 3, 14, 9, 4, 9, 8]
 
     expected = [3, 8, 5, 0, 5, 4, 7, 5, 4, 1, 3, 5, 9, 4, 9, 8]
@@ -35,53 +35,65 @@ class CreditCheckTest < Minitest::Test
   end
 
   def test_it_can_sum_numbers
-    credit_check = CreditCheck.new("4929735477250543")
+    credit_check = CreditCheck.new
     credit_card = [3, 8, 5, 0, 5, 4, 7, 5, 4, 1, 3, 5, 9, 4, 9, 8]
 
     assert_equal 80, credit_check.sum_numbers(credit_card)
   end
 
   def test_modulo_is_equal_to_zero_and_returns_true
-    credit_check = CreditCheck.new("4929735477250543")
+    credit_check = CreditCheck.new
 
-    assert credit_check.valid_number?("4929735477250543")
+    assert_equal true, credit_check.valid_number?(4929735477250543)
+  end
+
+  def test_modulo_is_equal_to_zero_and_returns_true_with_integer
+    credit_check = CreditCheck.new
+
+    assert_equal true, credit_check.valid_number?(4929735477250543)
   end
 
   def test_modulo_is_not_equal_to_zero_and_returns_false
-    credit_check = CreditCheck.new("5541801923795240")
+    credit_check = CreditCheck.new
 
-    refute credit_check.valid_number?("5541801923795240")
+    assert_equal false, credit_check.valid_number?(5541801923795240)
+  end
+
+  def test_modulo_is_not_equal_to_zero_and_returns_false_with_integer
+    credit_check = CreditCheck.new
+
+    refute credit_check.valid_number?(5541801923795240)
   end
 
   def test_it_will_generate_valid_message
-    credit_check = CreditCheck.new("4929735477250543")
+    credit_check = CreditCheck.new
 
     expected = "The number 4929735477250543 is valid."
 
-    assert_equal expected, credit_check.validation_output("4929735477250543")
+    assert_equal expected, credit_check.validation_output(4929735477250543)
   end
 
   def test_it_will_generate_invalid_message
-    credit_check = CreditCheck.new("5541801923795240")
+    credit_check = CreditCheck.new
 
     expected = "The number 5541801923795240 is invalid."
 
-    assert_equal expected, credit_check.validation_output("5541801923795240")
+    assert_equal expected, credit_check.validation_output(5541801923795240)
   end
 
   def test_it_validates_amex
-    credit_check = CreditCheck.new("342804633855673")
+    credit_check = CreditCheck.new
 
     expected = "The number 342804633855673 is valid."
 
-    assert_equal expected, credit_check.validation_output("342804633855673")
+    assert_equal expected, credit_check.validation_output(342804633855673)
   end
 
   def test_it_validates_invalid_amex
-    credit_check = CreditCheck.new("342801633855673")
+    credit_check = CreditCheck.new
 
     expected = "The number 342801633855673 is invalid."
 
-    assert_equal expected, credit_check.validation_output("342801633855673")
+    assert_equal expected, credit_check.validation_output(342801633855673)
   end
 end
